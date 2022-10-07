65900af7adebeaba8117302a387d5c11016db63c
Martin Toman <toman.martin@live.com>
2022-10-06 17:25:17 -0700

    Remove trefichi email

--- users/martin/email-accounts.nix
+++ users/martin/email-accounts.nix
@@ -32,34 +32,4 @@
     notmuch.enable = true;
     neomutt.enable = true;
   };
-
-  t_trefichi = rec {
-    address = "t@trefichi.com";
-    realName = "T";
-
-    userName = address;
-    passwordCommand = "${pkgs.pass}/bin/pass t@trefichi.com";
-
-    imap = {
-      host = "mail.privateemail.com";
-      port = 993;
-      tls.enable = true;
-    };
-    smtp = {
-      host = "mail.privateemail.com";
-      port = 587;
-      tls = {
-        enable = true;
-        useStartTls = true;
-      };
-    };
-
-    msmtp.enable = true;
-    mbsync = {
-      enable = true;
-      create = "maildir";
-    };
-    notmuch.enable = true;
-    neomutt.enable = true;
-  };
 }
